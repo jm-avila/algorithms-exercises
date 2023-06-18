9d3878a6b54f1f1dda22bce6f92cac6e2bf3aed3
jose-avila-007 <jose.avila@rookiewise.com>
2023-06-18 11:33:09 +0200

    search

--- specs/search/search.test.js
+++ specs/search/search.test.js
@@ -4,16 +4,35 @@
 // it's up to you what to return if the object isn't found (we're not testing that)
 
 function linearSearch(id, array) {
-  // code goes here
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].id === id) {
+      return array[i];
+    }
+  }
 }
 
 function binarySearch(id, array) {
-  // code goes here
+  const computeMiddleIndex = (length) => Math.floor(length / 2);
+  let middleIndex = computeMiddleIndex(array.length);
+  while (middleIndex) {
+    const item = array[middleIndex];
+    if (item.id === id) {
+      return item;
+    }
+    if (id < item.id) {
+      array = array.slice(0, middleIndex);
+    } else {
+      array = array.slice(middleIndex);
+    }
+    middleIndex = computeMiddleIndex(array.length);
+  }
+
+  return void 0;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("linear search", function () {
+test("linear search", function () {
   const lookingFor = { id: 5, name: "Brian" };
   expect(
     linearSearch(5, [
@@ -30,12 +49,12 @@ test.skip("linear search", function () {
       { id: 2, name: "Marc" },
       { id: 51, name: "Chris" },
       lookingFor,
-      { id: 14, name: "Ben" }
+      { id: 14, name: "Ben" },
     ])
   ).toBe(lookingFor);
 });
 
-test.skip("binary search", function () {
+test("binary search", function () {
   const lookingFor = { id: 23, name: "Brian" };
   expect(
     binarySearch(23, [
@@ -52,7 +71,7 @@ test.skip("binary search", function () {
       { id: 19, name: "Marc" },
       { id: 21, name: "Chris" },
       lookingFor,
-      { id: 24, name: "Ben" }
+      { id: 24, name: "Ben" },
     ])
   ).toBe(lookingFor);
 });
