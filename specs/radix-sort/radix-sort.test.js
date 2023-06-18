b5caeda99898947d28cacf2050a675ea37febf00
jose-avila-007 <jose.avila@rookiewise.com>
2023-06-18 11:01:25 +0200

    radix-sort

--- specs/radix-sort/radix-sort.test.js
+++ specs/radix-sort/radix-sort.test.js
@@ -9,60 +9,53 @@
 
 */
 
+const getLongestNumberLength = (array) => {
+  let greatestLength = 0;
+  array.forEach((num) => {
+    const numLength = num.toString().length;
+    if (numLength > greatestLength) {
+      greatestLength = numLength;
+    }
+  });
+  return greatestLength;
+};
+
 function radixSort(array) {
-  // code goes here
+  const longestNumberLength = getLongestNumberLength(array);
+  const buckets = Array.from(
+    {
+      length: 10, // base 10
+    },
+    () => []
+  );
+
+  for (let i = 0; i < longestNumberLength; i++) {
+    while (array.length) {
+      const item = array.shift().toString();
+      const bucketIndex = item[item.length - 1 - i] || 0;
+      buckets[bucketIndex].push(+item);
+    }
+    for (let j = 0; j < buckets.length; j++) {
+      array.push(...buckets[j]);
+      buckets[j] = [];
+    }
+  }
+
+  return array;
 }
 
 // unit tests
 // do not modify the below code
-describe.skip("radix sort", function () {
+describe("radix sort", function () {
   it("should sort correctly", () => {
     const nums = [
-      20,
-      51,
-      3,
-      801,
-      415,
-      62,
-      4,
-      17,
-      19,
-      11,
-      1,
-      100,
-      1244,
-      104,
-      944,
-      854,
-      34,
-      3000,
-      3001,
-      1200,
-      633
+      20, 51, 3, 801, 415, 62, 4, 17, 19, 11, 1, 100, 1244, 104, 944, 854, 34,
+      3000, 3001, 1200, 633,
     ];
     const ans = radixSort(nums);
     expect(ans).toEqual([
-      1,
-      3,
-      4,
-      11,
-      17,
-      19,
-      20,
-      34,
-      51,
-      62,
-      100,
-      104,
-      415,
-      633,
-      801,
-      854,
-      944,
-      1200,
-      1244,
-      3000,
-      3001
+      1, 3, 4, 11, 17, 19, 20, 34, 51, 62, 100, 104, 415, 633, 801, 854, 944,
+      1200, 1244, 3000, 3001,
     ]);
   });
   it("should sort 99 random numbers correctly", () => {
